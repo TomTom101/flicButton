d943e472c561feb4e83d193f01c0de98470780f0
Thomas Brandl <thobra@gmail.com>
2016-04-03 14:00:21 +0200

    Added start script for the daemon

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # flicButton
+
+chown pi:pi fliclifx.sh && chmod +x fliclifx.sh
+sudo cp fliclifx.sh /etc/init.d/
+sudo update-rc.d fliclifx.sh defaults
+/etc/init.d/fliclifx.sh start
---
+++ fliclifx.sh
@@ -0,0 +1,56 @@
+#!/bin/sh -
+
+### BEGIN INIT INFO
+# Provides:             fliclifx
+# Required-Start:
+# Required-Stop:
+# Default-Start:        2 3 4 5
+# Default-Stop:         0 1 6
+# Short-Description:    Run flicButton daemon
+### END INIT INFO
+
+DIR=/home/pi/flicButton
+DAEMON=${DIR}/daemon
+DAEMON_OPTS="-f ${DIR}/flic.sqlite3"
+DAEMON_NAME=fliclifx
+
+# This next line determines what user the script runs as.
+DAEMON_USER=root
+
+# The process ID of the script when it runs is stored here:
+PIDFILE=/var/run/$DAEMON_NAME.pid
+
+. /lib/lsb/init-functions
+
+do_start () {
+    log_daemon_msg "Starting system $DAEMON_NAME daemon"
+    start-stop-daemon --start --background --pidfile $PIDFILE --make-pidfile --user $DAEMON_USER --chuid $DAEMON_USER --startas $DAEMON -- $DAEMON_OPTS
+    log_end_msg $?
+}
+do_stop () {
+    log_daemon_msg "Stopping system $DAEMON_NAME daemon"
+    start-stop-daemon --stop --pidfile $PIDFILE --retry 10
+    log_end_msg $?
+}
+
+case "$1" in
+  start|stop)
+      do_${1}
+      ;;
+
+  restart|reload|force-reload)
+      do_stop
+      do_start
+      ;;
+
+  status)
+      status_of_proc "$DAEMON_NAME" "$DAEMON" && exit 0 || exit $?
+      ;;
+
+  *)
+
+  echo "Usage: /etc/init.d/fliclifx {start|stop}"
+  exit 1
+  ;;
+esac
+exit 0
